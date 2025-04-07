d08f1c837ce47a1e4c39556772b481e752da14be
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-04-07 12:09:59 +0200

    pull down updateQuality aged brie business logic from UpdatableItem to AgedBrieUpdatableItem subclass

--- src/UpdatableItem/AgedBrieUpdatableItem.php
+++ src/UpdatableItem/AgedBrieUpdatableItem.php
@@ -6,5 +6,14 @@ namespace GildedRose\UpdatableItem;
 
 final class AgedBrieUpdatableItem extends UpdatableItem 
 {
-    
+    private const MAXIMUM_ITEM_QUALITY = 50;
+
+    protected function updateQuality(): void
+    {
+        if ($this->quality >= self::MAXIMUM_ITEM_QUALITY) {
+            return;
+        }
+
+        $this->quality = $this->quality + 1;
+    }
 }
--- src/UpdatableItem/UpdatableItem.php
+++ src/UpdatableItem/UpdatableItem.php
@@ -7,7 +7,6 @@ namespace GildedRose\UpdatableItem;
 class UpdatableItem
 {
     private const MINIMUM_ITEM_QUALITY = 0;
-    private const MAXIMUM_ITEM_QUALITY = 50;
     private const AGED_BRIE_ITEM_NAME = 'Aged Brie';
     private const SULFURAS_ITEM_NAME = 'Sulfuras, Hand of Ragnaros';
     private const BACKSTAGE_PASSES_ITEM_NAME = 'Backstage passes to a TAFKAL80ETC concert';
@@ -79,15 +78,7 @@ class UpdatableItem
             return;
         }
 
-        if ($this->quality >= self::MAXIMUM_ITEM_QUALITY) {
-            return;
-        }
-
         $this->quality = $this->quality + 1;
-        
-        if($this->name !== self::BACKSTAGE_PASSES_ITEM_NAME) {
-            return;
-        }
         
         if ($this->sellIn >= 11) {
             return;
